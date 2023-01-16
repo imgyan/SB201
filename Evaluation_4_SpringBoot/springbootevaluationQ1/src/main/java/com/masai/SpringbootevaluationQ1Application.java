5f55d60bf02d24dd5e475e9e458a8037bd78fe33
imgyan <inex.gyan@gmail.com>
2023-01-16 23:59:07 +0530

    eval4

---
+++ Evaluation_4_SpringBoot/springbootevaluationQ1/src/main/java/com/masai/SpringbootevaluationQ1Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootevaluationQ1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootevaluationQ1Application.class, args);
+	}
+
+}
